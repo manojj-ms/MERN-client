36a694ce94c110ec1cd20f17d72b6447e3a7f84f
Manoj <manojkumarmsd9204@gmail.com>
2021-08-05 16:12:26 +0530

    commit message

---
+++ src/actions/types.js
@@ -0,0 +1,16 @@
+// defined some string constant that indicates the type of action being performed
+
+export const CREATE_TUTORIAL = "CREATE_TUTORIAL";
+export const RETRIEVE_TUTORIALS = "RETRIEVE_TUTORIALS";
+export const UPDATE_TUTORIAL = "UPDATE_TUTORIAL";
+export const DELETE_TUTORIAL = "DELETE_TUTORIAL";
+export const DELETE_ALL_TUTORIALS = "DELETE_ALL_TUTORIALS";
+
+export const REGISTER_SUCCESS = "REGISTER_SUCCESS";
+export const REGISTER_FAIL = "REGISTER_FAIL";
+export const LOGIN_SUCCESS = "LOGIN_SUCCESS";
+export const LOGIN_FAIL = "LOGIN_FAIL";
+export const LOGOUT = "LOGOUT";
+
+export const SET_MESSAGE = "SET_MESSAGE";
+export const CLEAR_MESSAGE = "CLEAR_MESSAGE";
